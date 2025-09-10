e303174dd6ef86d3e0ef2371f7418de645bb35e4
TYAGimanu1 <tyagipriyanshu43@gmail.com>
2025-09-10 10:46:34 +0530

    Updated api

--- api/user.js
+++ api/user.js
@@ -1,20 +1,13 @@
-import { readFile } from 'fs/promises';
-import path from 'path';
+const path = require('path');
+const fs = require('fs/promises');
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
+  const dbPath = path.join(__dirname, '..', 'db.json');
   try {
-    const filePath = path.join(process.cwd(), 'db.json');
-    const data = await readFile(filePath, 'utf-8');
+    const data = await fs.readFile(dbPath, 'utf-8');
     const jsonData = JSON.parse(data);
-
-    const userId = req.query.id; // Assuming the ID is passed as a query parameter
-    const user = jsonData.user.find(u => u.id === userId);
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).json({ error: 'User not found' });
-    }
+    res.status(200).json(jsonData.user);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to load data' });
+    res.status(500).json({ error: 'Failed to read data' });
   }
-}
+};
